82ea01c19b5fbca4b075cff2c2cc01f3d8fc7211
Krati Chauhan <chauhan.krati@gmail.com>
2020-06-23 21:57:33 -0700

    CRUD operations added on activity class

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,7 +13,7 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set;}
-
+        public DbSet<Activity> Activities { get; set;}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Value>()
